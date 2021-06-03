435c0528cd0e605fe868440b109982a5841d3889
GarotosDePrograma31 <78889337+GarotosDePrograma31@users.noreply.github.com>
2021-06-02 21:48:37 -0300

    Delete prova directory

--- prova/src/source/EmptyListException.java
+++
@@ -1,7 +0,0 @@
-package source;
-
-@SuppressWarnings("serial")
-public class EmptyListException extends RuntimeException {
-
-	public EmptyListException(String err) { super(err); }
-}
--- prova/src/source/Position.java
+++
@@ -1,6 +0,0 @@
-package source;
-
-public interface Position<E> {
-
-	E element();
-}
